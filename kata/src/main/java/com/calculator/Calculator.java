baa03d40b7270a42ef0cec818f7d315ac97357a7
Nikhil Bhat <nikhilbhat021@gmail.com>
2025-10-30 11:51:22 +0530

    Single input, Two inputs, Space inconsistency

--- kata/src/main/java/com/calculator/Calculator.java
+++ kata/src/main/java/com/calculator/Calculator.java
@@ -8,6 +8,13 @@ public class Calculator {
             return 0;
         
         int sum=0;
+        final String[] nums = input.split(",");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+            final int num = Integer.parseInt(nums[i].trim());
+            sum += num;
+        }
+
         return sum;
     }
 
--- kata/src/test/java/CalculatorTest.java
+++ kata/src/test/java/CalculatorTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.calculator.Calculator;
@@ -12,9 +13,27 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("testSimpleEmptyInputString")
     void testSimpleEmptyInputString() {
-        System.out.println("testSimpleEmptyInputString , NIKHIL c bhat");
         assertEquals(0, calcInstance.add(""), 0);
     }
 
+    @Test
+    @DisplayName("testSimpleSingleNumInputString")
+    void testSimpleSingleNumInputString() {
+        assertEquals(3, calcInstance.add("3"), 0);
+    }
+
+    @Test
+    @DisplayName("testSimpleTwoNumInputString")
+    void testSimpleTwoNumInputString() {
+        assertEquals(5, calcInstance.add("1,1, 3"), 0);
+    }
+
+    @Test
+    @DisplayName("testSimpleTwoNumWithSpaceInputString")
+    void testSimpleTwoNumWithSpaceInputString() {
+        System.out.println("testSimpleTwoNumInputString");
+        assertEquals(5, calcInstance.add("1,1, 3"), 0);
+    }
 }
